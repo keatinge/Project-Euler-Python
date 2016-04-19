bc0c2a7b245d5145355ce03bb2a7ac624c5839f0
keatinge <willy.keatinge@gmail.com>
2016-04-19 18:57:04 -0400

    fixed some wierd spacing

--- 19.py
+++ 19.py
@@ -1,8 +1,8 @@
 monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-monthNames = 	{0: "January", 1:"February", 2:"March", 3:"April", 4:"May", 5:"June",
+monthNames = {0: "January", 1:"February", 2:"March", 3:"April", 4:"May", 5:"June",
 				6:"July", 7:"August", 8:"September", 9:"October", 10:"November", 11:"December"}
 daysOfWeek = [0,1,2,3,4,5,6]
-daysOfWeekNames = 	{0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday"}
+daysOfWeekNames = {0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday"}
 
 
 #problem gives us Jan 1 1990 is a monday
